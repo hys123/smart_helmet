382b7b6add7cabadb85b4f41c68b2e2539437b07
cobraj <anjunn@163.co>
2020-05-31 20:45:36 +0800

    工程初始化

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "presets": [
+        ["es2015", { "modules": false }]
+    ],
+    "plugins": [
+        [
+            "component",
+            {
+                "libraryName": "element-ui",
+                "styleLibraryName": "theme-chalk"
+            }
+        ]
+    ]
+}
